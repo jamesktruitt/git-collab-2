05a3242ada85b8f7c53f6ae98c511006666f3417
jeff-burns <1jeffreyburns@gmail.com>
2018-06-13 13:26:08 -0600

    first

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 const newArray = [1, 2, 3, 4, 5]
+
+function multiplyArrayElements (newArray) {
+    var total=1;
+    for (var i=0; i<newArray.length; i++) {
+        total = total * newArray[i];
+    } 
+    return total;
+}
